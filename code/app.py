a249bad855049b4d4453d138d703137f77644728
Antonis <antonistodoris@gmail.com>
2019-03-12 10:32:55 +0100

    Created POST, GET for items

--- code/app.py
+++ code/app.py
@@ -1,14 +1,32 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.secret_key = 'Antonis'
 api = Api(app)
+
+items = []
 
 
 class Student(Resource):
   def get(self, name):
-    return {'student': name}
+    item = next(filter(lambda x: x['name'] == name, items), None)
+    return {'item': item}, 200 if item else 404
 
-api.add_resource(Student, '/student/<string:name>') #http://localhost:5000/student/Antonis
+  def post(self, name):
+    if next(filter(lambda x: x['name'] == name, items), None):
+      return {'message': "An item with name '{}' already exists".format(name)}, 400
+    data = request.get_json()
+    item = {'name': name, 'price': data['price']}
+    items.append(item)
+    return item, 201
 
-app.run(port=5000)
+class ItemList(Resource):
+  def get(self):
+    return {'items': items}
+
+
+api.add_resource(Student, '/item/<string:name>') #http://localhost:5000/student/Antonis
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
